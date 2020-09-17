efe73f0fbad8c3fbf0968b221d6c5b85e6189e31
Roney Cacau <roneycacau@gmail.com>
2020-09-17 20:31:26 -0300

    fix aluno

--- src/lista4/exercicio2/Temperatura.java
+++ src/lista4/exercicio2/Temperatura.java
@@ -12,46 +12,18 @@ public class Temperatura {
         System.out.println(temperaturas.toString());
 
         temperaturas.inserirInicio(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirInicio(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirInicio(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirInicio(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirInicio(random.doubles(-100, 100 ).findFirst().getAsDouble());
         System.out.println(temperaturas.toString());
+
         temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
-        temperaturas.inserirFinal(random.doubles(-100, 100 ).findFirst().getAsDouble());
+        System.out.println(temperaturas.toString());
+
         temperaturas.inserirQualquer(random.doubles(-100, 100 ).findFirst().getAsDouble(), 2);
-        temperaturas.inserirQualquer(random.doubles(-100, 100 ).findFirst().getAsDouble(), 2);
-        temperaturas.inserirQualquer(random.doubles(-100, 100 ).findFirst().getAsDouble(), 2);
-        temperaturas.inserirQualquer(random.doubles(-100, 100 ).findFirst().getAsDouble(), 2);
-        temperaturas.inserirQualquer(random.doubles(-100, 100 ).findFirst().getAsDouble(), 2);
+        System.out.println(temperaturas.toString());
+
         temperaturas.removerInicio();
-        temperaturas.removerInicio();
-        temperaturas.removerInicio();
-        temperaturas.removerInicio();
-        temperaturas.removerInicio();
+
         temperaturas.removerFinal();
-        temperaturas.removerFinal();
-        temperaturas.removerFinal();
-        temperaturas.removerFinal();
-        temperaturas.removerFinal();
-        temperaturas.removerQualquer(random.nextInt(6)+1);
-        temperaturas.removerQualquer(random.nextInt(6)+1);
-        temperaturas.removerQualquer(random.nextInt(6)+1);
-        temperaturas.removerQualquer(random.nextInt(6)+1);
+
         temperaturas.removerQualquer(random.nextInt(6)+1);
 
         System.out.println(temperaturas.toString());
--- src/lista4/exercicio3/TesteAluno.java
+++ src/lista4/exercicio3/TesteAluno.java
@@ -5,19 +5,11 @@ import lista4.util.Lista;
 import java.util.Random;
 
 public class TesteAluno {
+        static Random random = new Random();
     public static void main(String[] args) {
-        Random random = new Random();
-        int numeruzin = random.ints(1, 5)
-                .findFirst()
-                .getAsInt();
-        String sobrenome = numeruzin == 1 ? "Silva" :
-                numeruzin == 2 ? "Santos" :
-                        numeruzin == 3 ? "Oliveira" :
-                                numeruzin == 4 ? "Alves" : "Orleans e Bragança";
-
-        String turma = "Turma " + numeruzin;
-        String semestre = numeruzin + "º";
-        String nome = "Aluno " + sobrenome + " " + sobrenome;
+        String turma = "Turma " + numeruzin();
+        String semestre = numeruzin() + "º";
+        String nome = "Aluno " + sobrenome() + " " + sobrenome();
 
 
         Aluno aluno = new Aluno(nome, turma, semestre);
@@ -67,5 +59,17 @@ public class TesteAluno {
         alunos.removerQualquer(random.nextInt(6) + 1);
 
         alunos.toString();
+    }
+
+    public static int numeruzin(){
+        return random.ints(1, 5)
+                .findFirst()
+                .getAsInt();
+    }
+    public static String sobrenome(){
+        return numeruzin() == 1 ? "Silva" :
+                numeruzin() == 2 ? "Santos" :
+                        numeruzin() == 3 ? "Oliveira" :
+                                numeruzin() == 4 ? "Alves" : "Orleans e Bragança";
     }
 }
